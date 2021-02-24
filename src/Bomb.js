fa554893fb2ceae55a3f88d39b855d2d00397e99
John Ratana <jratana1@gmail.com>
2021-02-24 00:17:34 -0500

    commit tree 8b68e9b757f0d1ed49bce83cd56fdef4d6450420

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }  
+
+    timerText = () => {
+        if (this.state.secondsLeft === 0) {
+          return <h4>Boom!</h4>;
+            }
+        return <h4>{this.state.secondsLeft} seconds left before I go boom!</h4>;
+    
+    }
+
+  render() {
+    return (
+      <div className="cbomb">
+          {this.timerText()}
+      </div>
+    );
+  }
+}
